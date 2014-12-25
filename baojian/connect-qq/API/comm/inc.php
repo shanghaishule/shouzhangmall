a7ec696509c778b71659b1baab62927b65438016
ranyigit <ranyi_num@163.com>
2014-12-25 15:44:19 +0800

    qq互联信息

--- baojian/connect-qq/API/comm/inc.php
+++ baojian/connect-qq/API/comm/inc.php
@@ -1,2 +1,2 @@
 <?php die('forbidden'); ?>
-{"appid":"这里填写appid","appkey":"这里填写appkey","callback":"http://www.81bjmall.com/user-login-connect_qq.html","scope":"get_user_info","errorReport":true,"storageType":"file","host":"localhost","user":"root","password":"root","database":"test"}
+{"appid":"10118091","appkey":"578e562a4f8b403a3d41fab18ac77f34","callback":"http://www.81bjmall.com/user-login-connect_qq.html","scope":"get_user_info","errorReport":true,"storageType":"file","host":"localhost","user":"root","password":"root","database":"test"}
