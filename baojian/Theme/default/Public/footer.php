4ccc6477efb02d399c8f8e4bd0c75cc6337a95be
ranyigit <ranyi_num@163.com>
2014-12-29 16:02:41 +0800

    网站统计

--- baojian/Theme/default/Public/footer.php
+++ baojian/Theme/default/Public/footer.php
@@ -38,4 +38,5 @@
 <?php echo mc_xihuan_js(); ?>
 <?php echo mc_shoucang_js(); ?>
 <?php echo mc_guanzhu_js(); ?>
+<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1253993139'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s95.cnzz.com/z_stat.php%3Fid%3D1253993139' type='text/javascript'%3E%3C/script%3E"));</script>
 </html>
