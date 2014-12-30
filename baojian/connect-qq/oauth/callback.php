ed898ad898a4790d06025aa02d204dfa9c884fb1
ranyigit <ranyi_num@163.com>
2014-12-30 15:15:46 +0800

    测试

--- baojian/connect-qq/oauth/callback.php
+++ baojian/connect-qq/oauth/callback.php
@@ -7,7 +7,7 @@ $acs = $qc->qq_callback();
 $oid = $qc->get_openid();
 $qc = new QC($acs,$oid);
 //$uinfo = $qc->get_user_info();
-$page_id = M('meta')->where("meta_key='user_qqoid' AND meta_value='".$oid."' AND type='user'")->getField('page_id');
+//$page_id = M('meta')->where("meta_key='user_qqoid' AND meta_value='".$oid."' AND type='user'")->getField('page_id');
 function mc_check_user_name($name) {
 	$user_login = M('meta')->where("meta_key='user_login' AND type ='user'")->getField('meta_value',true);
 	if(in_array($name, $user_login)){
