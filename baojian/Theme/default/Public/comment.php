91396f15cdeb4a7b48183ed60d2aa3b122b619b5
ranyigit <ranyi_num@163.com>
2015-04-13 10:34:18 +0800

    去掉商品评论时间

--- baojian/Theme/default/Public/comment.php
+++ baojian/Theme/default/Public/comment.php
@@ -8,7 +8,7 @@
 			<h4 class="media-heading">
 				<a href="<?php echo U('user/index/index?id='.$val['user_id']); ?>"><?php echo mc_user_display_name($val['user_id']); ?></a>
 				<?php if(mc_get_meta($val['user_id'],'user_level',true,'user')==10) : ?><span class="btn btn-danger btn-xs">管理员</span><?php elseif(mc_comment_group($val['id']) && mc_get_meta($val['user_id'],'group_admin',true,'user')==mc_comment_group($val['id'])) : ?><span class="btn btn-info btn-xs">群组管理员</span><?php endif; ?>
-				<small class="pull-right"><?php echo date('Y-m-d H:i:s',$val['date']); ?></small>
+				<!-- --<small class="pull-right"><?php echo date('Y-m-d H:i:s',$val['date']); ?></small> -->
 				<?php if(mc_is_admin()) : ?><a class="btn btn-danger btn-xs pull-right" href="<?php echo U('home/perform/comment_delete?id='.$val['id']); ?>">删除</a><?php elseif(mc_comment_group($val['id']) && mc_get_meta($val['user_id'],'group_admin',true,'user')==mc_comment_group($val['id'])) : ?><?php endif; ?>
 			</h4>
 			<?php echo mc_magic_out($val['action_value']); ?>
